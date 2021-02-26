60d63deaedd9457e4137a874c6b00d36d7768188
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2021-02-26 17:23:44 +0000

    fix running dom4me
    git-svn-id: https://svn.code.sf.net/p/domination/code@1991 5f7a89d5-bd68-4938-9893-cab8262575d4

--- Domination/test/demo_flash.sh
+++ Domination/test/demo_flash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "`dirname "$0"`"
-java -Ddebug=true -cp ../lib/midletrunner.jar:../lib/m3gbasic.jar:../lib/SwingME.jar:../lib/LobbyClient.jar:../lib/Grasshopper.jar:../dist/Domination4ME.jar org.me4se.MIDletRunner -width 320 -height 480 net.yura.domination.mobile.flashgui.DominationMain
+java -Ddebug=true -cp ../swingUI/lib/midletrunner.jar:../swingUI/lib/m3gbasic.jar:../swingUI/lib/SwingME.jar:../swingUI/lib/LobbyClient.jar:../swingUI/lib/Grasshopper.jar:../swingUI/dist/Domination4ME.jar org.me4se.MIDletRunner -width 320 -height 480 net.yura.domination.mobile.flashgui.DominationMain
 
