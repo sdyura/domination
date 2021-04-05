e61f8b4a5d5bc3dda41cfdd7e69f87c4b32d7339
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2021-04-05 15:12:25 +0000

    move to androidx part 2
    git-svn-id: https://svn.code.sf.net/p/domination/code@2027 5f7a89d5-bd68-4938-9893-cab8262575d4

--- Domination/sharedUI/src_mapstore_lobby/net/yura/lobby/mini/MiniLobbyClient.java
+++ Domination/sharedUI/src_mapstore_lobby/net/yura/lobby/mini/MiniLobbyClient.java
@@ -424,7 +424,7 @@ public class MiniLobbyClient implements LobbyClient,ActionListener {
 
         // GCM push only works on android
         if (Midlet.getPlatform()==Midlet.PLATFORM_ANDROID) {
-            Midlet.openURL("nativeNoResult://net.yura.domination.android.GCMActivity");
+            Midlet.openURL("nativeNoResult://net.yura.domination.android.push.GCMActivity");
         }
 
         mycom.getGameTypes();
@@ -725,7 +725,7 @@ public class MiniLobbyClient implements LobbyClient,ActionListener {
     public void setUserInfo(String user,java.util.List info) { }
 
     /**
-     * @see net.yura.domination.android.GCMIntentService#onMessage(android.content.Context, android.content.Intent)
+     * @see net.yura.domination.android.push.GCMIntentService#onMessage(android.content.Context, android.content.Intent)
      */
     static void notify(Game game, boolean onlyBackground) {
 
