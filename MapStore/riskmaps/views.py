41ccb4d1701815dda6cf75dac973a3da0c040868
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2022-06-26 12:17:45 +0000

    sending form mail fix
    git-svn-id: https://svn.code.sf.net/p/domination/code@2217 5f7a89d5-bd68-4938-9893-cab8262575d4

--- MapStore/riskmaps/views.py
+++ MapStore/riskmaps/views.py
@@ -236,11 +236,13 @@ def upload_unauthorised_map(request):
             newMap.save()
             mapForm.save_m2m()
 
+            import logging
             try:
                 import mechanize
                 br = mechanize.Browser()
                 r = br.open('http://msg.yura.net/cgi-sys/FormMail.cgi', 'recipient=yura@yura.net&subject=NewMap&email=newmap@maps.yura.net&mapId=' + str(newMap.id) + '&mapName=' + newMap.name)
             except:
+                logging.exception("formmail error")
                 pass # Failed email is not a priority
 
             return redirect('/')
