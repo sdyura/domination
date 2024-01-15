0c2923f7bbfd78814c7305aef3336d0f4ddaa559
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2024-01-15 22:30:08 +0000

    simple audio
    git-svn-id: https://svn.code.sf.net/p/domination/code@2481 5f7a89d5-bd68-4938-9893-cab8262575d4

---
+++ Domination/sharedUI/src_mapstore_lobby/net/yura/domination/SimpleAudio.java
@@ -0,0 +1,75 @@
+package net.yura.domination;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.microedition.media.Manager;
+import javax.microedition.media.MediaException;
+import javax.microedition.media.Player;
+import javax.microedition.media.PlayerListener;
+import net.yura.domination.audio.AudioSystem;
+
+public class SimpleAudio implements AudioSystem, PlayerListener {
+
+    private static final Logger LOGGER = Logger.getLogger(SimpleAudio.class.getName());
+    
+    Map<String, Player> currentPlayers = new HashMap(); // filename -> player
+
+    private Player getPlayer(String fileName) throws IOException, MediaException {
+
+        // TODO we need some sort of cache, as each time java seems to load it from scratch
+        // i guess it makes sense as the file or url destination may have changed from last time
+        // so we need to keep the last X audio samples used
+
+        //Player player = Manager.createPlayer(RiskUtil.openStream(fileName), "audio/basic");
+        Player player = Manager.createPlayer("file:///android_asset/" + fileName);
+        player.addPlayerListener(this); // TODO do we need this?
+        return player;
+    }
+
+    public void play(String fileName) {
+        try {
+            Player player = getPlayer(fileName);
+            player.start();
+        }
+        catch (Exception ex) {
+            LOGGER.log(Level.WARNING, "unable to play " + fileName, ex);
+        }
+    }
+
+    @Override
+    public void start(String fileName) {
+        try {
+            Player player = getPlayer(fileName);
+            player.setLoopCount(-1);
+            currentPlayers.put(fileName, player);
+            player.start();
+        }
+        catch (Exception ex) {
+            LOGGER.log(Level.WARNING, "unable to play " + fileName, ex);
+        }
+    }
+
+    @Override
+    public void playerUpdate(Player player, String event, Object eventData) {
+        System.out.println("meow " + event);
+    }
+
+    @Override
+    public void stop(String audioFile) {
+        try {
+            Player player = currentPlayers.remove(audioFile);
+            if (player != null) {
+                player.stop();
+            }
+            else {
+                LOGGER.log(Level.INFO, "unable to find " + audioFile);
+            }
+        }
+        catch (Exception ex) {
+            LOGGER.log(Level.WARNING, "unable to stop " + audioFile, ex);
+        }
+    }
+}
