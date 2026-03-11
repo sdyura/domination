b7bfa887b5edfc9ec8a21c74b74f843d29a189b3
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2026-03-11 13:51:54 +0000

    fix try catch location
    git-svn-id: https://svn.code.sf.net/p/domination/code@2735 5f7a89d5-bd68-4938-9893-cab8262575d4

--- Domination/ios/src/main/java/net/yura/domination/ios/DominationApplication.java
+++ Domination/ios/src/main/java/net/yura/domination/ios/DominationApplication.java
@@ -5,7 +5,6 @@ import net.yura.ios.SwingMEiOSApplication;
 import net.yura.lobby.client.LobbySettings;
 import net.yura.lobby.util.SimplePreferences;
 import java.io.File;
-import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 import apple.foundation.c.Foundation;
 import apple.foundation.enums.NSSearchPathDirectory;
@@ -21,34 +20,34 @@ public class DominationApplication {
         String lobbyUUID = sharedDefaults.stringForKey("uuid");
 */
 
-        String group = "GP37R5KJ29.net.yura.lobby";
-        String service = null;
+        try {
+            String group = "GP37R5KJ29.net.yura.lobby";
+            String service = null;
 
-        // clear
-        //KeychainUtil.delete(group, service, LobbySettings.UUID_KEY);
+            // clear
+            //KeychainUtil.delete(group, service, LobbySettings.UUID_KEY);
 
-        String keychainUUID = KeychainUtil.load(group, service, LobbySettings.UUID_KEY);
-        String documentsDirectory = Foundation.NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).firstObject();
-        Preferences prop = new SimplePreferences(new File(documentsDirectory, LobbySettings.LOBBY_OLD_SETTINGS_FILE), null);
-        String appFilesUUID = prop.get(LobbySettings.UUID_KEY, null);
+            String keychainUUID = KeychainUtil.load(group, service, LobbySettings.UUID_KEY);
+            String documentsDirectory = Foundation.NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).firstObject();
+            Preferences prop = new SimplePreferences(new File(documentsDirectory, LobbySettings.LOBBY_OLD_SETTINGS_FILE), null);
+            String appFilesUUID = prop.get(LobbySettings.UUID_KEY, null);
 
-        // check if id exists in the keychain
-        if (keychainUUID != null) {
-            // if it exists then save it to the lobby file
-            if (!keychainUUID.equals(appFilesUUID)) {
-                prop.put(LobbySettings.UUID_KEY, keychainUUID);
-
-                try {
+            // check if id exists in the keychain
+            if (keychainUUID != null) {
+                // if it exists then save it to the lobby file
+                if (!keychainUUID.equals(appFilesUUID)) {
+                    prop.put(LobbySettings.UUID_KEY, keychainUUID);
                     prop.flush();
-                } catch (BackingStoreException e) {
-                    e.printStackTrace();
                 }
             }
+            // check if lobby id exists,
+            else if (appFilesUUID != null) {
+                // if it exists then save it to the keychain
+                KeychainUtil.save(group, service, LobbySettings.UUID_KEY, appFilesUUID);
+            }
         }
-        // check if lobby id exists,
-        else if (appFilesUUID != null) {
-            // if it exists then save it to the keychain
-            KeychainUtil.save(group, service, LobbySettings.UUID_KEY, appFilesUUID);
+        catch (Throwable e) {
+            e.printStackTrace();
         }
 
         SwingMEiOSApplication.main(args);
